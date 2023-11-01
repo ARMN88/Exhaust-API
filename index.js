7851a382d0ca86120e1778eaf3dfc715a09e8d33
ARMN88 <56323614+ARMN88@users.noreply.github.com>
2023-11-01 10:51:20 -0500

    Added Oil Catch Can

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ let transporter = nodemailer.createTransport({
 
 let downpipePrice = 1000;
 let wingPrice = 1000;
+let oilCatchCanPrice = 1000;
 
 const downpipeUrl =
   'https://www.amsperformance.com/product/infiniti-q60-q50-vr30ddtt-red-alpha-lower-downpipes/';
@@ -22,12 +23,16 @@ const downpipeUrl =
 const wingUrl =
   'https://jaliscoscarbonfiber.com/products/infiniti-q50-q60-m-style-carbon-fiber-spoiler?variant=41071722823880';
 
+const oilCatchCanUrl =
+  'https://www.z1motorsports.com/z1-products/z1-motorsports/z1-q50-q60-30t-baffled-oil-catch-can-kit-p-23546.html';
+
 const handle = setInterval(function () {
   downpipeScrape();
   wingScrape();
+  oilCatchCanScrape();
 }, 1000 * 60);
 
-async function downpipeScrape() {
+async function downpipeScrape(isFirst = false) {
   const { data } = await axios.get(downpipeUrl);
 
   const $ = cheerio.load(data);
@@ -39,16 +44,18 @@ async function downpipeScrape() {
   );
 
   if (newPrice < downpipePrice) {
-    await transporter.sendMail({
-      from: `Downpipe Bot" <${process.env.EMAIL}>`,
-      to: '7087013449@vtext.com',
-      text: `Downpipe was $${downpipePrice}, now $${newPrice}!`,
-    });
+    if (!isFirst) {
+      await transporter.sendMail({
+        from: `Downpipe Bot" <${process.env.EMAIL}>`,
+        to: '7087013449@vtext.com',
+        text: `Downpipe was $${downpipePrice}, now $${newPrice}!`,
+      });
+    }
     downpipePrice = newPrice;
   }
 }
 
-async function wingScrape() {
+async function wingScrape(isFirst = false) {
   const { data } = await axios.get(wingUrl);
 
   const $ = cheerio.load(data);
@@ -58,14 +65,38 @@ async function wingScrape() {
   const newPrice = parseFloat($(container).find('.money').text().substring(1));
 
   if (newPrice < wingPrice) {
-    await transporter.sendMail({
-      from: `Wing Bot" <${process.env.EMAIL}>`,
-      to: '7087013449@vtext.com',
-      text: `Wing was $${wingPrice}, now $${newPrice}!`,
-    });
+    if (!isFirst) {
+      await transporter.sendMail({
+        from: `Wing Bot" <${process.env.EMAIL}>`,
+        to: '7087013449@vtext.com',
+        text: `Wing was $${wingPrice}, now $${newPrice}!`,
+      });
+    }
     wingPrice = newPrice;
   }
 }
 
-downpipeScrape();
-wingScrape();
+async function oilCatchCanScrape(isFirst = false) {
+  const { data } = await axios.get(wingUrl);
+
+  const $ = cheerio.load(data);
+
+  const container = $('#ProductPrice');
+
+  const newPrice = parseFloat($(container).find('.money').text().substring(1));
+
+  if (newPrice < oilCatchCanPrice) {
+    if (!isFirst) {
+      await transporter.sendMail({
+        from: `Oil Catch Can Bot" <${process.env.EMAIL}>`,
+        to: '7087013449@vtext.com',
+        text: `Oil Catch Can was $${oilCatchCanPrice}, now $${newPrice}!`,
+      });
+    }
+    oilCatchCanPrice = newPrice;
+  }
+}
+
+downpipeScrape(true);
+wingScrape(true);
+oilCatchCanScrape(true);
