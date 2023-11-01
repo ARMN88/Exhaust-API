b990d96e3ead76d8a7a08a1cf2f25b3a21718874
ARMN88 <56323614+ARMN88@users.noreply.github.com>
2023-10-31 23:12:38 -0500

    Finished Wing & Pipe

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
-let exhaustPrice = Infinity;
+let transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+});
 
-const url =
+let downpipePrice = 1000;
+let wingPrice = 1000;
+
+const downpipeUrl =
   'https://www.amsperformance.com/product/infiniti-q60-q50-vr30ddtt-red-alpha-lower-downpipes/';
 
-const handle = setInterval(scrape, 1000 * 60 * 12);
+const wingUrl =
+  'https://jaliscoscarbonfiber.com/products/infiniti-q50-q60-m-style-carbon-fiber-spoiler?variant=41071722823880';
 
-async function scrape() {
-  const { data } = await axios.get(url);
+const handle = setInterval(function () {
+  downpipeScrape();
+  wingScrape();
+}, 1000 * 60);
+
+async function downpipeScrape() {
+  const { data } = await axios.get(downpipeUrl);
 
   const $ = cheerio.load(data);
 
@@ -19,8 +38,34 @@ async function scrape() {
     $(container).find('.price bdi').text().substring(1)
   );
 
-  if (newPrice < exhaustPrice) {
+  if (newPrice < downpipePrice) {
+    await transporter.sendMail({
+      from: `Downpipe Bot" <${process.env.EMAIL}>`,
+      to: '7087013449@vtext.com',
+      text: `Downpipe was $${downpipePrice}, now $${newPrice}!`,
+    });
+    downpipePrice = newPrice;
   }
 }
 
-scrape();
+async function wingScrape() {
+  const { data } = await axios.get(wingUrl);
+
+  const $ = cheerio.load(data);
+
+  const container = $('#ProductPrice');
+
+  const newPrice = parseFloat($(container).find('.money').text().substring(1));
+
+  if (newPrice < wingPrice) {
+    await transporter.sendMail({
+      from: `Wing Bot" <${process.env.EMAIL}>`,
+      to: '7087013449@vtext.com',
+      text: `Wing was $${wingPrice}, now $${newPrice}!`,
+    });
+    wingPrice = newPrice;
+  }
+}
+
+downpipeScrape();
+wingScrape();
