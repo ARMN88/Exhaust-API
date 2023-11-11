62db10cb4547368e41ae238b5aa0692f4af0a09d
ARMN88 <56323614+ARMN88@users.noreply.github.com>
2023-11-10 23:40:07 -0600

    Added Scraper Class

---
+++ Scraper.js
@@ -0,0 +1,53 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+let transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+});
+
+class Scraper {
+  constructor(name, url, number, container, selector) {
+    this.name = name;
+    this.url = url;
+    this.number = number;
+
+    this.container = container;
+    this.selector = selector;
+    this.price = Infinity;
+
+    this.scrape(true);
+  }
+
+  async scrape(isFirst = false) {
+    const { data } = await axios.get(this.url);
+
+    const $ = cheerio.load(data);
+
+    const container = $(this.container);
+
+    const newPrice = parseFloat(
+      $(container).find(this.selector).text().substring(1)
+    );
+
+    if (newPrice < this.price) {
+      if (!isFirst) {
+        await transporter.sendMail({
+          from: `${this.name} Bot" <${process.env.EMAIL}>`,
+          to: this.number,
+          text: `${this.name} was $${this.price}, now $${newPrice}!\n${this.url}`,
+        });
+      }
+      this.price = newPrice;
+    }
+  }
+}
+
+module.exports = Scraper;
--- index.js
+++ index.js
@@ -1,102 +1,41 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const nodemailer = require('nodemailer');
-require('dotenv').config();
+const Scraper = require('./Scraper.js');
 
-let transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASS,
-  },
-});
+const rafat = '7087013449@vtext.com';
+const ashraf = '7085514450@tmomail.net';
 
-let downpipePrice = 1000;
-let wingPrice = 1000;
-let oilCatchCanPrice = 1000;
+const scrapers = [
+  new Scraper(
+    'Downpipe',
+    'https://www.amsperformance.com/product/infiniti-q60-q50-vr30ddtt-red-alpha-lower-downpipes/',
+    rafat,
+    '.elementor-element-08e529f',
+    '.price bdi'
+  ),
+  new Scraper(
+    'Wing',
+    'https://jaliscoscarbonfiber.com/products/infiniti-q50-q60-m-style-carbon-fiber-spoiler?variant=41071722823880',
+    rafat,
+    '#ProductPrice',
+    '.money'
+  ),
+  new Scraper(
+    'Oil Catch Can',
+    'https://www.z1motorsports.com/z1-products/z1-motorsports/z1-q50-q60-30t-baffled-oil-catch-can-kit-p-23546.html',
+    rafat,
+    '#display_price_bottom',
+    '.sp-newPrice'
+  ),
+  new Scraper(
+    'Coilovers',
+    'https://zzperformance.com/products/zzp-cadillac-ats-coilovers',
+    ashraf,
+    '.product__price',
+    '.visually-hidden'
+  ),
+];
 
-const downpipeURL =
-  'https://www.amsperformance.com/product/infiniti-q60-q50-vr30ddtt-red-alpha-lower-downpipes/';
-
-const wingURL =
-  'https://jaliscoscarbonfiber.com/products/infiniti-q50-q60-m-style-carbon-fiber-spoiler?variant=41071722823880';
-
-const oilCatchCanURL =
-  'https://www.z1motorsports.com/z1-products/z1-motorsports/z1-q50-q60-30t-baffled-oil-catch-can-kit-p-23546.html';
-
-const handle = setInterval(function () {
-  downpipeScrape();
-  wingScrape();
-  oilCatchCanScrape();
-}, 1000 * 60);
-
-async function downpipeScrape(isFirst = false) {
-  const { data } = await axios.get(downpipeURL);
-
-  const $ = cheerio.load(data);
-
-  const container = $('.elementor-element-08e529f');
-
-  const newPrice = parseFloat(
-    $(container).find('.price bdi').text().substring(1)
-  );
-
-  if (newPrice < downpipePrice) {
-    if (!isFirst) {
-      await transporter.sendMail({
-        from: `Downpipe Bot" <${process.env.EMAIL}>`,
-        to: '7087013449@vtext.com',
-        text: `Downpipe was $${downpipePrice}, now $${newPrice}!\n${downpipeURL}`,
-      });
-    }
-    downpipePrice = newPrice;
-  }
-}
-
-async function wingScrape(isFirst = false) {
-  const { data } = await axios.get(wingURL);
-
-  const $ = cheerio.load(data);
-
-  const container = $('#ProductPrice');
-
-  const newPrice = parseFloat($(container).find('.money').text().substring(1));
-
-  if (newPrice < wingPrice) {
-    if (!isFirst) {
-      await transporter.sendMail({
-        from: `Wing Bot" <${process.env.EMAIL}>`,
-        to: '7087013449@vtext.com',
-        text: `Wing was $${wingPrice}, now $${newPrice}!\n${wingURL}`,
-      });
-    }
-    wingPrice = newPrice;
-  }
-}
-
-async function oilCatchCanScrape(isFirst = false) {
-  const { data } = await axios.get(oilCatchCanURL);
-
-  const $ = cheerio.load(data);
-
-  const container = $('#display_price_bottom');
-
-  const newPrice = parseFloat($(container).find('.sp-newPrice').text().substring(1));
-
-  if (newPrice < oilCatchCanPrice) {
-    if (!isFirst) {
-      await transporter.sendMail({
-        from: `Oil Catch Can Bot" <${process.env.EMAIL}>`,
-        to: '7087013449@vtext.com',
-        text: `Oil Catch Can was $${oilCatchCanPrice}, now $${newPrice}!\n${oilCatchCanURL}`,
-      });
-    }
-    oilCatchCanPrice = newPrice;
-  }
-}
-
-downpipeScrape(true);
-wingScrape(true);
-oilCatchCanScrape(true);
+const handle = setInterval(async () => {
+  scrapers.forEach(async (scraper) => {
+    await scraper.scrape();
+  });
+}, 1000 * 60 * 60);
